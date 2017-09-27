d60921169fe674bd6e993771c853625b25583181
Sebastian Wojnicz <sebulw@gmail.com>
2017-09-27 16:45:48 +0200

    script created

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -1,7 +1,10 @@
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
-	dinosaur = 'triceratops';
-	dinosaurUpperCased = dinosaur.toUpperCase();
-	velociraptorReplaced = text.replace('Velociraptor', dinosaurUpperCased);
-var textLength = text.length/2;
-var textLength = velociraptorReplaced.slice(0, textLength);
-console.log(textLength)
+function drawTree(treeHeight) {
+	for (var i = 0; i < treeHeight; i++) {
+		var star = "";
+		for (var j = i ; j < i; j++) {
+			star += "*";
+		}
+		console.log(star);
+	}
+}
+drawTree(5);
